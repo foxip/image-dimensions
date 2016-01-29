301789c80a86cd2b496b88ff03019197c5e9b87a
Albert Vos <albert@foxip.net>
2016-01-29 10:37:23 +0100

    Rename variable

--- image-dimensions/ImageScanner.cs
+++ image-dimensions/ImageScanner.cs
@@ -11,28 +11,28 @@ namespace Foxip.Image.Dimensions
             ImageInfo imageInfo = new ImageInfo();
 
             // Read the first 30 bytes
-            byte[] b = new byte[30];
-            if (stream.Read(b, 0, b.Length) != b.Length)
+            byte[] buffer = new byte[30];
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length)
             {
                 imageInfo.Error = "Too small";
                 return imageInfo;
             }
 
-            string s = Encoding.UTF8.GetString(b);
+            string s = Encoding.UTF8.GetString(buffer);
 
             // Determine image format
             if (s.StartsWith("GIF87a") || s.StartsWith("GIF89a"))
                 imageInfo.ImageFormat = ImageFormat.Gif;
-            else if ((b[0] == 0xFF) && (b[1] == 0xD8))
+            else if ((buffer[0] == 0xFF) && (buffer[1] == 0xD8))
                 imageInfo.ImageFormat = ImageFormat.Jpg;
-            else if ((b[0] == 0x89) &&
-                     (b[1] == 0x50) &&
-                     (b[2] == 0x4e) &&
-                     (b[3] == 0x47) &&
-                     (b[4] == 0x0d) &&
-                     (b[5] == 0x0a) &&
-                     (b[6] == 0x1a) &&
-                     (b[7] == 0x0a))
+            else if ((buffer[0] == 0x89) &&
+                     (buffer[1] == 0x50) &&
+                     (buffer[2] == 0x4e) &&
+                     (buffer[3] == 0x47) &&
+                     (buffer[4] == 0x0d) &&
+                     (buffer[5] == 0x0a) &&
+                     (buffer[6] == 0x1a) &&
+                     (buffer[7] == 0x0a))
                 imageInfo.ImageFormat = ImageFormat.Png;
             else if (s.StartsWith("BM"))
                 imageInfo.ImageFormat = ImageFormat.Bmp;
@@ -48,17 +48,17 @@ namespace Foxip.Image.Dimensions
             switch (imageInfo.ImageFormat)
             {
                 case ImageFormat.Gif:
-                    imageInfo.Width = BitConverter.ToUInt16(b, 6);
-                    imageInfo.Height = BitConverter.ToUInt16(b, 8);
+                    imageInfo.Width = BitConverter.ToUInt16(buffer, 6);
+                    imageInfo.Height = BitConverter.ToUInt16(buffer, 8);
                     break;
                 case ImageFormat.Bmp:
-                    imageInfo.Width = BitConverter.ToUInt16(b, 18);
-                    imageInfo.Height = BitConverter.ToUInt16(b, 22);
+                    imageInfo.Width = BitConverter.ToUInt16(buffer, 18);
+                    imageInfo.Height = BitConverter.ToUInt16(buffer, 22);
                     break;
                 case ImageFormat.Psd:
 
                     // PSD version number should always be 1
-                    if ((b[4] != 0 || b[5] != 1))
+                    if ((buffer[4] != 0 || buffer[5] != 1))
                     {
                         imageInfo.Error = "Invalid PSD format";
                         return imageInfo;
@@ -68,15 +68,15 @@ namespace Foxip.Image.Dimensions
                     byte[] psdWidth = new byte[4];
                     byte[] psdHeight = new byte[4];
 
-                    psdHeight[0] = b[17];
-                    psdHeight[1] = b[16];
-                    psdHeight[2] = b[15];
-                    psdHeight[3] = b[14];
+                    psdHeight[0] = buffer[17];
+                    psdHeight[1] = buffer[16];
+                    psdHeight[2] = buffer[15];
+                    psdHeight[3] = buffer[14];
 
-                    psdWidth[0] = b[21];
-                    psdWidth[1] = b[20];
-                    psdWidth[2] = b[19];
-                    psdWidth[3] = b[18];
+                    psdWidth[0] = buffer[21];
+                    psdWidth[1] = buffer[20];
+                    psdWidth[2] = buffer[19];
+                    psdWidth[3] = buffer[18];
 
                     imageInfo.Width = BitConverter.ToInt32(psdWidth, 0);
                     imageInfo.Height = BitConverter.ToInt32(psdHeight, 0);
@@ -95,15 +95,15 @@ namespace Foxip.Image.Dimensions
                     byte[] pngWidth = new byte[4];
                     byte[] pngHeight = new byte[4];
 
-                    pngWidth[0] = b[19];
-                    pngWidth[1] = b[18];
-                    pngWidth[2] = b[17];
-                    pngWidth[3] = b[16];
+                    pngWidth[0] = buffer[19];
+                    pngWidth[1] = buffer[18];
+                    pngWidth[2] = buffer[17];
+                    pngWidth[3] = buffer[16];
 
-                    pngHeight[0] = b[23];
-                    pngHeight[1] = b[22];
-                    pngHeight[2] = b[21];
-                    pngHeight[3] = b[20];
+                    pngHeight[0] = buffer[23];
+                    pngHeight[1] = buffer[22];
+                    pngHeight[2] = buffer[21];
+                    pngHeight[3] = buffer[20];
 
                     imageInfo.Width = BitConverter.ToInt32(pngWidth, 0);
                     imageInfo.Height = BitConverter.ToInt32(pngHeight, 0);
@@ -116,10 +116,10 @@ namespace Foxip.Image.Dimensions
                     int offset = 0;
                     string state = "";
 
-                    while ((true) && (pos < b.Length))
+                    while ((true) && (pos < buffer.Length))
                     { 
                         // Marker starts with FF
-                        if (b[pos] != 0xFF)
+                        if (buffer[pos] != 0xFF)
                         {
                             imageInfo.Error = "Invalid JPG format";
                             return imageInfo;
@@ -127,24 +127,24 @@ namespace Foxip.Image.Dimensions
 
                         // Marker type
                         pos++;
-                        marker = b[pos];
+                        marker = buffer[pos];
 
                         // Length
                         pos++;
-                        b[0] = b[pos + 1]; // Reverse byte
-                        b[1] = b[pos]; // Reverse byte
-                        len = BitConverter.ToUInt16(b, 0);
+                        buffer[0] = buffer[pos + 1]; // Reverse byte
+                        buffer[1] = buffer[pos]; // Reverse byte
+                        len = BitConverter.ToUInt16(buffer, 0);
 
                         // Read more from the stream
-                        offset = b.Length;
-                        b = Resize(b, b.Length + len);
+                        offset = buffer.Length;
+                        buffer = Resize(buffer, buffer.Length + len);
                         int bytesToRead = len;
-                        int bytesRead = stream.Read(b, offset, len);
+                        int bytesRead = stream.Read(buffer, offset, len);
                         while (bytesToRead != bytesRead)
                         {
                             bytesToRead = (ushort)(bytesToRead - bytesRead);
                             offset = offset + bytesRead;
-                            bytesRead = stream.Read(b, offset, bytesToRead);
+                            bytesRead = stream.Read(buffer, offset, bytesToRead);
                         }
 
                         // SOF Marker for width/height
@@ -153,13 +153,13 @@ namespace Foxip.Image.Dimensions
                             ((marker >= 0xC9) && (marker <= 0xCB)) ||
                             ((marker >= 0xCD) && (marker <= 0xCF)))
                         {
-                            b[0] = b[pos + 4]; // Reverse byte
-                            b[1] = b[pos + 3]; // Reverse byte
-                            imageInfo.Height = BitConverter.ToInt16(b, 0);
+                            buffer[0] = buffer[pos + 4]; // Reverse byte
+                            buffer[1] = buffer[pos + 3]; // Reverse byte
+                            imageInfo.Height = BitConverter.ToInt16(buffer, 0);
 
-                            b[0] = b[pos + 6]; // Reverse byte
-                            b[1] = b[pos + 5]; // Reverse byte
-                            imageInfo.Width = BitConverter.ToInt16(b, 0);
+                            buffer[0] = buffer[pos + 6]; // Reverse byte
+                            buffer[1] = buffer[pos + 5]; // Reverse byte
+                            imageInfo.Width = BitConverter.ToInt16(buffer, 0);
                             break;
                         }
 
